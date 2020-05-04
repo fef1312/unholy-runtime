adcf357227cf6c703e71acfa4f253afe38b1628d
Felix Kopp <sandtler@sandtler.club>
2020-05-05 00:36:23 +0200

    Make output fancier

--- src/utils/ast-walk.ts
+++ src/utils/ast-walk.ts
@@ -70,7 +70,7 @@ export type WalkCallback = (current: Node, depth: number, name: string, leaf?: s
  * @param cb A callback that will be invoked for every tree node.
  */
 export default function astWalk(sourceFile: SourceFile, cb: WalkCallback): void {
-    cb(sourceFile, 0, `SourceFile <${sourceFile.fileName}>`, "file");
+    cb(sourceFile, 0, `SourceFile ( \x1b[1m${sourceFile.fileName}\x1b[0m )`, "file");
     sourceFile.statements.forEach(stmt => visitStatement(cb, stmt, 1, "statement"));
 }
 
@@ -95,28 +95,28 @@ function visitStatement(cb: WalkCallback, node: Statement, depth: number, leaf?:
             visitExpressionStatement(cb, node as ExpressionStatement, depth, leaf);
             return;
     }
-    cb(node, depth, "Statement < ~unimplemented~ >", leaf);
+    cb(node, depth, "\x1b[36mStatement\x1b[0m ( \x1b[1m~unimplemented~\x1b[0m )", leaf);
 }
 
 function visitBlockStatement(cb: WalkCallback, node: BlockStatement, depth: number, leaf?: string) {
-    cb(node, depth, "BlockStatement", leaf);
+    cb(node, depth, "\x1b[36mBlockStatement\x1b[0m", leaf);
     node.statements.forEach(stmt => visitStatement(cb, stmt, depth + 1, "statement"));
 }
 
 function visitVarDeclarationStatement(cb: WalkCallback, node: VarDeclarationStatement,
                                       depth: number, leaf?: string) {
-    cb(node, depth, "VarDeclarationStatement", leaf);
+    cb(node, depth, "\x1b[36mVarDeclarationStatement\x1b[0m", leaf);
     visitVarDeclaration(cb, node.declaration, depth + 1, "declaration");
 }
 
 function visitFuncDeclarationStatement(cb: WalkCallback, node: FuncDeclarationStatement,
                                        depth: number, leaf?: string) {
-    cb(node, depth, "FuncDeclarationStatement", leaf);
+    cb(node, depth, "\x1b[36mFuncDeclarationStatement\x1b[0m", leaf);
     visitFuncDeclaration(cb, node.declaration, depth + 1, "declaration");
 }
 
 function visitIfStatement(cb: WalkCallback, node: IfStatement, depth: number, leaf?: string) {
-    cb(node, depth, "IfStatement", leaf);
+    cb(node, depth, "\x1b[36mIfStatement\x1b[0m", leaf);
     visitExpression(cb, node.condition, depth + 1, "condition");
     visitStatement(cb, node.thenStatement, depth + 1, "thenStatement");
     if (node.elseStatement !== undefined) {
@@ -125,7 +125,7 @@ function visitIfStatement(cb: WalkCallback, node: IfStatement, depth: number, le
 }
 
 function visitReturnStatement(cb: WalkCallback, node: ReturnStatement, depth: number, leaf?: string) {
-    cb(node, depth, "ReturnStatement", leaf);
+    cb(node, depth, "\x1b[36mReturnStatement\x1b[0m", leaf);
     if (node.expression !== undefined) {
         visitExpression(cb, node.expression, depth + 1, "expression");
     }
@@ -133,12 +133,12 @@ function visitReturnStatement(cb: WalkCallback, node: ReturnStatement, depth: nu
 
 function visitExpressionStatement(cb: WalkCallback, node: ExpressionStatement, depth: number,
                                   leaf?: string) {
-    cb(node, depth, "ExpressionStatement", leaf);
+    cb(node, depth, "\x1b[36mExpressionStatement\x1b[0m", leaf);
     visitExpression(cb, node.expression, depth + 1, "expression");
 }
 
 function visitVarDeclaration(cb: WalkCallback, node: VarDeclaration, depth: number, leaf?: string) {
-    cb(node, depth, "VarDeclaration", leaf);
+    cb(node, depth, "\x1b[33mVarDeclaration\x1b[0m", leaf);
     visitIdentifier(cb, node.name, depth + 1, "name");
     if (node.type !== undefined) {
         visitType(cb, node.type, depth + 1, "type");
@@ -150,7 +150,7 @@ function visitVarDeclaration(cb: WalkCallback, node: VarDeclaration, depth: numb
 
 function visitFuncDeclaration(cb: WalkCallback, node: FuncDeclaration, depth: number,
                               leaf?: string) {
-    cb(node, depth, "FuncDeclaration", leaf);
+    cb(node, depth, "\x1b[33mFuncDeclaration\x1b[0m", leaf);
     visitIdentifier(cb, node.name, depth + 1, "name");
     for (let i = 0; i < node.params.length; i++) {
         visitParameterDeclaration(cb, node.params[i], depth + 1, `params[${i}]`);
@@ -161,13 +161,13 @@ function visitFuncDeclaration(cb: WalkCallback, node: FuncDeclaration, depth: nu
 
 function visitParameterDeclaration(cb: WalkCallback, node: ParameterDeclaration, depth: number,
                                   leaf?: string) {
-    cb(node, depth, "ParameterDeclaration", leaf);
+    cb(node, depth, "\x1b[33mParameterDeclaration\x1b[0m", leaf);
     visitIdentifier(cb, node.name, depth + 1, "name");
     visitType(cb, node.type, depth + 1, "type");
 }
 
 function visitIdentifier(cb: WalkCallback, node: Identifier, depth: number, leaf?: string) {
-    cb(node, depth, `Identifier < ${node.name} >`, leaf);
+    cb(node, depth, `\x1b[96mIdentifier\x1b[0m ( \x1b[1m${node.name}\x1b[0m )`, leaf);
 }
 
 function visitType(cb: WalkCallback, node: TypeNode, depth: number, leaf?: string) {
@@ -175,14 +175,14 @@ function visitType(cb: WalkCallback, node: TypeNode, depth: number, leaf?: strin
         case SyntaxKind.BoolKeyword:
         case SyntaxKind.IntKeyword:
         case SyntaxKind.VoidKeyword:
-            cb(node, depth, `TypeNode < ${tokenToString((node as KeywordTypeNode).kind)} >`, leaf);
+            cb(node, depth, `\x1b[32mTypeNode\x1b[0m ( \x1b[1m${tokenToString((node as KeywordTypeNode).kind)}\x1b[0m )`, leaf);
             return;
     }
-    cb(node, depth, "TypeNode < ~unimplemented~ >");
+    cb(node, depth, "\x1b[32mTypeNode\x1b[0m ( \x1b[1m~unimplemented~\x1b[0m )");
 }
 
 function visitToken(cb: WalkCallback, node: TokenNode<SyntaxKind>, depth: number, leaf?: string) {
-    cb(node, depth, `Token < ${tokenToString(node.kind)} >`, leaf);
+    cb(node, depth, `\x1b[92mToken\x1b[0m ( \x1b[1m${tokenToString(node.kind)}\x1b[0m )`, leaf);
 }
 
 function visitExpression(cb: WalkCallback, node: Expression, depth: number, leaf?: string) {
@@ -198,26 +198,26 @@ function visitExpression(cb: WalkCallback, node: Expression, depth: number, leaf
             return;
         case SyntaxKind.TrueKeyword:
         case SyntaxKind.FalseKeyword:
-            cb(node, depth, `BoolLiteral < ${tokenToString(node.kind)} >`, leaf);
+            cb(node, depth, `\x1b[95mBoolLiteral\x1b[0m ( \x1b[1m${tokenToString(node.kind)}\x1b[0m )`, leaf);
             return;
         case SyntaxKind.CallExpression:
             visitCallExpression(cb, node as CallExpression, depth, leaf);
             return;
     }
 
-    cb(node, depth, "Expression < ~unimplemented~ >", leaf);
+    cb(node, depth, "\x1b[93mExpression\x1b[0m ( \x1b[1m~unimplemented~\x1b[0m )", leaf);
 }
 
 function visitBinaryExpression(cb: WalkCallback, node: BinaryExpression, depth: number,
                                leaf?: string) {
-    cb(node, depth, "BinaryExpression", leaf);
+    cb(node, depth, "\x1b[93mBinaryExpression\x1b[0m", leaf);
     visitExpression(cb, node.left, depth + 1, "left");
     visitToken(cb, node.operatorToken, depth + 1, "operatorToken");
     visitExpression(cb, node.right, depth + 1, "right");
 }
 
 function visitCallExpression(cb: WalkCallback, node: CallExpression, depth: number, leaf?: string) {
-    cb(node, depth, "CallExpression", leaf);
+    cb(node, depth, "\x1b[93mCallExpression\x1b[0m", leaf);
     visitExpression(cb, node.callee, depth + 1, "callee");
     for (let i = 0; i < node.args.length; i++) {
         visitExpression(cb, node.args[i], depth + 1, `args[${i}]`);
@@ -225,5 +225,5 @@ function visitCallExpression(cb: WalkCallback, node: CallExpression, depth: numb
 }
 
 function visitIntegerLiteral(cb: WalkCallback, node: IntegerLiteral, depth: number, leaf?: string) {
-    cb(node, depth, `IntegerLiteral < ${node.text} >`, leaf);
+    cb(node, depth, `\x1b[95mIntegerLiteral\x1b[0m ( \x1b[1m${node.text}\x1b[0m )`, leaf);
 }
